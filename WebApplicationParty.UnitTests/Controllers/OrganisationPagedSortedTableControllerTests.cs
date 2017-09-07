9a65b80cb5024653a8865cc5bec1ba9a165985a2
Steven Alexander <stevena@kainos.com>
2017-09-07 12:12:57 +0100

    added tests for paged controllers

---
+++ WebApplicationParty.UnitTests/Controllers/OrganisationPagedSortedTableControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using WebApplicationParty.Controllers;
+using PartyData.Repositories;
+using PartyData.Entities;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationParty.Models;
+using System.Threading.Tasks;
+using PartyData.Data;
+
+namespace WebApplicationParty.UnitTests.Controllers
+{
+    [TestFixture]
+    public class OrganisationPagedSortedTableControllerTests
+    {
+        private OrganisationPagedSortedTableController _controller;
+        private IPagedSortedRepository<OrganisationResultItem> _pagedSortedRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pagedSortedRepository = Substitute.For<IPagedSortedRepository<OrganisationResultItem>>();
+            _controller = new OrganisationPagedSortedTableController(_pagedSortedRepository);
+        }
+
+        [Test]
+        public async Task Index_GetsDataAndReturnsView()
+        {
+            var pagedSortedResult = new PagedSortedResult<OrganisationResultItem>()
+            {
+                recordsFiltered = 50,
+                recordsTotal = 100,
+                data = new List<OrganisationResultItem>(),
+            };
+            
+            _pagedSortedRepository.GetPagedSortedResults(0, 10, "Name", true).Returns(pagedSortedResult);
+            
+            var result = await _controller.Index();
+
+            Assert.IsTrue(result is ViewResult);
+            Assert.AreEqual(pagedSortedResult.recordsTotal, ((result as ViewResult).Model as PagedSortedViewModel<OrganisationResultItem>).RecordsTotal);
+            Assert.AreEqual(pagedSortedResult.recordsFiltered, ((result as ViewResult).Model as PagedSortedViewModel<OrganisationResultItem>).RecordsFiltered);
+            Assert.AreSame(pagedSortedResult.data, ((result as ViewResult).Model as PagedSortedViewModel<OrganisationResultItem>).Data);
+        }
+    }
+}
---
+++ WebApplicationParty.UnitTests/Controllers/PersonPagedSortedTableControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using WebApplicationParty.Controllers;
+using PartyData.Repositories;
+using PartyData.Entities;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationParty.Models;
+using System.Threading.Tasks;
+using PartyData.Data;
+
+namespace WebApplicationParty.UnitTests.Controllers
+{
+    [TestFixture]
+    public class PersonPagedSortedTableControllerTests
+    {
+        private PersonPagedSortedTableController _controller;
+        private IPagedSortedRepository<PersonResultItem> _pagedSortedRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pagedSortedRepository = Substitute.For<IPagedSortedRepository<PersonResultItem>>();
+            _controller = new PersonPagedSortedTableController(_pagedSortedRepository);
+        }
+
+        [Test]
+        public async Task Index_GetsDataAndReturnsView()
+        {
+            var pagedSortedResult = new PagedSortedResult<PersonResultItem>()
+            {
+                recordsFiltered = 50,
+                recordsTotal = 100,
+                data = new List<PersonResultItem>(),
+            };
+            
+            _pagedSortedRepository.GetPagedSortedResults(0, 10, "Name", true).Returns(pagedSortedResult);
+            
+            var result = await _controller.Index();
+
+            Assert.IsTrue(result is ViewResult);
+            Assert.AreEqual(pagedSortedResult.recordsTotal, ((result as ViewResult).Model as PagedSortedViewModel<PersonResultItem>).RecordsTotal);
+            Assert.AreEqual(pagedSortedResult.recordsFiltered, ((result as ViewResult).Model as PagedSortedViewModel<PersonResultItem>).RecordsFiltered);
+            Assert.AreSame(pagedSortedResult.data, ((result as ViewResult).Model as PagedSortedViewModel<PersonResultItem>).Data);
+        }
+    }
+}
